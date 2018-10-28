5978b3f2d9aaf7e7d5e54706e1fe99953151db46
Lubin <lubinkerhuel@yahoo.fr>
2018-10-28 21:34:05 +0100

    Fixed Time Step issue (PWM trig the time step). Find a factor 2 in QEI speed block output.

--- Experimentations/Hardware_setup/InitTPV0.m
+++ Experimentations/Hardware_setup/InitTPV0.m
@@ -14,7 +14,7 @@ PWMmax = 3199;
 % Temps mort
 Tm = 0.5e-6; % en s
 % Periode d'echantillonnage
-Ts = 1e-4;
+Ts = TPWM;  % We synchronize model Time Stpe with PWM (see ADC peripheral settings). This is 5e-4(s);
 Tsmeca = 1e-2;
 TsQEI = Tsmeca/5;   % QEI sampled 5 time faster than Tsmeca for filtering
 
@@ -24,29 +24,35 @@ Vdc = 24;
 Lignes = 250*4;
 
 
+% use filter designed using FDAtool (graphical interface)
+% coefs are set in block directly.
 
 %% Filtre vitesse version de Lubin
-try
-    LPF_vit_Fc = 50;  % 50Hz
-    [b,a] = butter(1,LPF_vit_Fc*TsQEI*2);    % Filter at 50 Hz (normalized frequency: 1 is Fnyquist)
-     fct = tf(b,a,Ts);
-    % figure(1); bode(fct);
-    % figure(2); step(fct);
-catch % if control toolbox not available
-    b = [0.2452    0.2452];
-    a = [1.0000   -0.5095];
+if 0
+    try
+        LPF_vit_Fc = 50;  % 50Hz
+        [b,a] = butter(1,LPF_vit_Fc*TsQEI*2);    % Filter at 50 Hz (normalized frequency: 1 is Fnyquist)
+        % fct = tf(b,a,Ts);
+        % figure(1); bode(fct);
+        % figure(2); step(fct);
+    catch % if control toolbox not available
+        b = [0.2452    0.2452];
+        a = [1.0000   -0.5095];
+    end
 end
 
 %% Filtre vitesse version fonctionnelle pour le moment, a verifier
-try
-    LPF_vit_Fc = 50;  % 50Hz
-    [b,a] = butter(1,LPF_vit_Fc*Ts*2);    % Filter at 50 Hz (normalized frequency: 1 is Fnyquist)
-    % fct = tf(b,a,Ts);
-    % figure(1); bode(fct);
-    % figure(2); step(fct);
-catch % if control toolbox not available
-    b = [0.0155    0.0155];
-    a = [1.0000   -0.9691];
+if 0
+    try
+        LPF_vit_Fc = 50;  % 50Hz
+        [b,a] = butter(1,LPF_vit_Fc*Ts*2);    % Filter at 50 Hz (normalized frequency: 1 is Fnyquist)
+        % fct = tf(b,a,Ts);
+        % figure(1); bode(fct);
+        % figure(2); step(fct);
+    catch % if control toolbox not available
+        b = [0.0155    0.0155];
+        a = [1.0000   -0.9691];
+    end
 end
 
 
