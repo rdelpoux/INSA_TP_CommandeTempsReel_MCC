76e4cf5143b186e598cd4f5b5b2d5ccdc16c1ab5
Lubin <lubinkerhuel@yahoo.fr>
2018-11-12 23:54:27 +0100

    Improved motor model format + added BEMF modulation (like sine wave on current measurement). Checked against real measurements

--- Experimentations/Log_2018_10_28/sim2mat.m
+++ Experimentations/Log_2018_10_28/sim2mat.m
@@ -1,7 +1,17 @@
 function sim2mat()
-% Convert Picgui Logs files from model MCLV2_dsPIC33EP256MC506_externalOpAmp_DataLog
+% Convert Picgui log files from model MCLV2_dsPIC33EP256MC506_externalOpAmp_DataLog
 % into input file to feed simulink model
+% Picgui log files are raw UART output stream sent to the PC by the model executed on the board.
+% the binary file (with txt extension) is then decoded and stored into a mat file with the picgui "load" button.
+% This script takes the resulting .mat file.
+%
+% The script convert input files into simulink format with a discrete time resolution of 50us.
+% Under-sampled sensors are padded with a zero-order hold filter (i.e. stairs)
 
+
+% list input "picugi" .mat log files:
+% output files with have same name preceded with 'Sim_'.
+% conversion not done if output file already exist (save time)
 Fnames = {...  
             '2018_10_28_at_23h48_37s',...   %% Steps 
             '2018_11_07_at_18h40_54s',...   %% Chirp
@@ -16,12 +26,13 @@ for iF = Fnames
         disp(['Load ' iF{1} '.mat']);
         load([iF{1} '.mat']);
     
+        % see modle for reference: MCLV2_dsPIC33EP256MC506_externalOpAmp_DataLog
         % 1st column is the PWM motor cmd (normalized in [-1 1], datatype in model is sfix16 en15) 
         %       sampled every 50us
         % 2nd column is Current measurement (A) with format: datatype in model is fixdt(1,16,4.4/32768,0) 
         %       sampled every 50us
-        % where 4.4/32768 is the LSB representation        
-        %       3rd column is the raw speed measurement from QEI peripehral. sampled every 2ms
+        %       where 4.4/32768 is the LSB representation        
+        % 3rd column is the raw speed measurement from QEI peripehral. sampled every 2ms
 
         A = padr(Rtotal,2); % trig on 2nd column all data are rewriten at 50us rate. 3rd column is sampled and hold.
         t = 0:.00005:((length(A)-1)*.00005);    % time vector for sim reference
